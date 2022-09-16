3fc844d573cf815944a13a647b16acd5c2df1b29
Susan Wairimu <mwihaki2susan@gmail.com>
2022-09-16 05:39:00 +0300

    includes()

---
+++ Includes.js
@@ -0,0 +1,11 @@
+/**
+ * includes() used to chech if a value/string is present in an array and returns true or false
+ * it is case sensitive
+ * */
+ 
+let ages = [23,18,36,20,24,27,19]
+
+console.log(ages.includes(20))
+
+let greeting = "Hello World, enjoy your day"
+console.log(greeting.includes("world"))
