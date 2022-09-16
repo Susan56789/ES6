de929920c131f19b2b5fad8e6145ac26a6805d1f
Susan Wairimu <mwihaki2susan@gmail.com>
2022-09-16 06:00:54 +0300

    padStart(), padEnd()

---
+++ padStart_padEnd.js
@@ -0,0 +1,13 @@
+/**
+ * padStart() is a string method used to pad the start of a string with a specific
+ * string to a certain length
+ * */
+
+let text = "Youtube tutorial"
+
+
+console.log(text.padStart(50,'r'))
+console.log(text.padStart(50,'r').length)
+
+console.log(text.padEnd(40,"a"))
+console.log(text.padEnd(40,"a").length)
