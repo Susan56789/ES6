dbc4cff4b2145dc541033bfd652876e4e51ea843
Susan Wairimu <mwihaki2susan@gmail.com>
2022-09-16 05:08:22 +0300

    arrow function

---
+++ ArrowFunction.js
@@ -0,0 +1,12 @@
+/**
+ * Arrow function allow us to write shorter function syntax
+ * */
+ 
+//funcion called multiply with arguments x,y
+ let multiply = (x,y) => console.log(x*y);
+multiply(5,4)
+
+ //function with no argument
+
+ let greeting = () => console.log("Hello World");
+ greeting()
